f068ca72f6bb3e3af57088a75c5a7af84f64bbb9
bishoy7000 <bishoy7000@gmail.com>
2016-05-13 10:17:19 +0300

    Update Product.java

--- src/rest_web/Product.java
+++ src/rest_web/Product.java
@@ -1,6 +1,7 @@
 package rest_web;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,19 +29,24 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 @Path("/product")
 public class Product {
+	private String db_url="jdbc:mysql://localhost:3306/products_db";
+	private String db_user="root";
+	public String db_password="";
+
+
 	  @Path("/select_products")
 	  @Produces("application/json")
 	  public JSONArray products_select() throws JSONException, SQLException, IOException {
 		  	JSONArray jarray = new JSONArray();
-		  	MysqlDataSource ds = null; 
+		  	MysqlDataSource db = null; 
 	        Connection connect = null; 
 	        Statement query =null;
 		  try {
-			  ds = new MysqlDataSource(); 
-	          ds.setUrl("jdbc:mysql://localhost:3306/products_db"); 
-	  	      ds.setUser("root"); 
-	          ds.setPassword(""); 
-	          connect = ds.getConnection(); 
+			  db = new MysqlDataSource(); 
+	          db.setUrl(db_url); 
+	  	      db.setUser(db_user); 
+	          db.setPassword(db_password); 
+	          connect = db.getConnection(); 
    		      query = connect.createStatement();
 			  ResultSet list = query.executeQuery("select * from products");
 				  while (list.next()) {
@@ -62,9 +68,14 @@ public class Product {
 
 		    try { query.close(); } catch (SQLException e) { e.printStackTrace(); } 
             try { connect.close(); } catch (SQLException e) { e.printStackTrace(); } 
+            try (FileWriter file = new FileWriter("D:\\test2.json")) {
+    			file.write(jarray.toString());
+            }
             System.out.println("Invoked");
 		  return jarray;
 	  }
+	  
+	  
 	  @POST
 	  @Path("/addProduct")
 	  @Consumes(MediaType.APPLICATION_JSON)
@@ -83,15 +94,15 @@ public class Product {
 			System.out.println("Data Received: " + product_details.toString());
 			JSONObject f = new JSONObject(product_details.toString());
 			
-			MysqlDataSource ds = null; 
+			MysqlDataSource db = null; 
 	        Connection connect = null; 
 	        PreparedStatement query =null;
 			 try {
-				  ds = new MysqlDataSource(); 
-		          ds.setUrl("jdbc:mysql://localhost:3306/products_db"); 
-		  	      ds.setUser("root"); 
-		          ds.setPassword(""); 
-		          connect = ds.getConnection(); 
+				  db = new MysqlDataSource(); 
+		          db.setUrl(db_url); 
+		  	      db.setUser(db_user); 
+		          db.setPassword(db_password); 
+		          connect = db.getConnection(); 
 	  		      query = connect.prepareStatement("INSERT INTO products (Name, Description, Price)VALUES (?,?,?)");
 	  		      query.setString(1, f.getString("Name"));
 	  		      query.setString(2, f.getString("Description"));
@@ -104,6 +115,8 @@ public class Product {
 			 try { query.close(); } catch (SQLException e) { e.printStackTrace(); } 
 	         try { connect.close(); } catch (SQLException e) { e.printStackTrace(); }
 	}
+	  
+	  
 	  @POST
 	  @Path("/editProduct")
 	  @Consumes(MediaType.APPLICATION_JSON)
@@ -121,15 +134,15 @@ public class Product {
 			System.out.println("Data Received: " + product_details.toString());
 			JSONObject f = new JSONObject(product_details.toString());
 			
-			MysqlDataSource ds = null; 
+			MysqlDataSource db = null; 
 	        Connection connect = null; 
 	        PreparedStatement query =null;
 			 try {
-				  ds = new MysqlDataSource(); 
-		          ds.setUrl("jdbc:mysql://localhost:3306/products_db"); 
-		  	      ds.setUser("root"); 
-		          ds.setPassword(""); 
-		          connect = ds.getConnection(); 
+				  db = new MysqlDataSource(); 
+		          db.setUrl(db_url); 
+		  	      db.setUser(db_user); 
+		          db.setPassword(db_password); 
+		          connect = db.getConnection(); 
 	  		      query = connect.prepareStatement("UPDATE products SET Name= ?, Description= ?, Price= ? WHERE ID= ?");
 	  		      query.setString(1, f.getString("Name"));
 	  		      query.setString(2, f.getString("Description"));
@@ -143,18 +156,20 @@ public class Product {
 			 try { query.close(); } catch (SQLException e) { e.printStackTrace(); } 
 	         try { connect.close(); } catch (SQLException e) { e.printStackTrace(); }
 	}
+	  
+	  
 	  @GET
 	  @Path("/deleteProduct/{id}")
 	  public Response  product_delete(@PathParam("id") int id ) {
-		  	MysqlDataSource ds = 		null; 
+		  	MysqlDataSource db = 		null; 
 	        Connection connect = 		null; 
 	        PreparedStatement query =	null;
 			 try {
-				  ds = new MysqlDataSource(); 
-		          ds.setUrl("jdbc:mysql://localhost:3306/products_db"); 
-		  	      ds.setUser("root"); 
-		          ds.setPassword(""); 
-		          connect = ds.getConnection(); 
+				  db = new MysqlDataSource(); 
+		          db.setUrl("jdbc:mysql://localhost:3306/products_db"); 
+		  	      db.setUser("root"); 
+		          db.setPassword(""); 
+		          connect = db.getConnection(); 
 	  		      query = connect.prepareStatement("DELETE FROM products WHERE ID= ?");
 	  		      query.setInt(1, id);
 	  		      query.executeUpdate();
@@ -164,6 +179,42 @@ public class Product {
 			 try { query.close(); } catch (SQLException e) { e.printStackTrace(); } 
 	         try { connect.close(); } catch (SQLException e) { e.printStackTrace(); }
 	         return Response.status(200).build();
+	}
+	  
+	  
+	  @GET
+	  @Path("/selectProduct/{id}")
+	  public String select(@PathParam("id") int id) {
+		  MysqlDataSource db = 		null; 
+	        Connection connect = 		null; 
+	        PreparedStatement query =	null;
+	        JSONObject jobj = new JSONObject();
+			 try {
+				  db = new MysqlDataSource(); 
+				  db.setUrl(db_url); 
+		  	      db.setUser(db_user); 
+		          db.setPassword(db_password);  
+		          connect = db.getConnection(); 
+	  		      query = connect.prepareStatement("SELECT * FROM products WHERE ID= ?");
+	  		      query.setInt(1, id);
+	  		    ResultSet product = query.executeQuery();
+	  		  if (product.next()) {
+				  String  name_json= 	   product.getString("Name");
+				  String discription_json= product.getString("Description");
+				  int price_json = 		   product.getInt("Price");
+				  int id_json = 		   product.getInt("ID");
+				    jobj.put("Name", 		name_json);
+				    jobj.put("Description", discription_json);
+				    jobj.put("Price", 		price_json);
+				    jobj.put("ID", 			id_json);
+				    System.out.println("Hello");
+				}
+			 }catch(Exception ex) {
+				   ex.printStackTrace();
+				}
+			 try { query.close(); } catch (SQLException e) { e.printStackTrace(); } 
+	         try { connect.close(); } catch (SQLException e) { e.printStackTrace(); }
+	         return jobj.toString();
 	}
 }
 
